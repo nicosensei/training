c0d31d1575398b2917c268ae64bf862a2e609b60
nicosensei <nicosensei@gmail.com>
2012-11-12 14:04:30 +0100

    First hello world app.

---
+++ AndroidTrainingSession082012/Hello Andy/src/fr/nikokode/helloandy/MainActivity.java
@@ -0,0 +1,26 @@
+package fr.nikokode.helloandy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void onClickBye(View view) {
+    	System.exit(0);
+    }
+    
+}
